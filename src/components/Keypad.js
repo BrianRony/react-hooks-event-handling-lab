c32e249c70f35ab236a086297075d81a2978ae7d
Brian Ngetich <kipronobrian97@gmail.com>
2024-05-22 08:00:49 +0300

    Event Handlers

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,16 @@
-// Code Keypad Component Here
+import React from "react";
 
 function Keypad (){
+    function handleChange(event) {
+        event.preventDefault();
+        console.log('Entering password...');
+    }
     return (
-        <div></div>
+        <div>
+            <form onChange={handleChange}> 
+            <input type="password" />
+            </form>
+        </div>
     )
 }
 
